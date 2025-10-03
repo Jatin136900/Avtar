3c5d2b8109a277b53ed4c4719f34498674f4834c
Jatin <jatinverma2428@gmail.com>
2025-10-03 14:42:48 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import Avtar from "./Avtar";
 import TodoList from "./TodoList";
 import Box from "./Box";
+import img from "./images/car1.webp";
+import img2 from "./images/car2.webp";
+import img3 from "./images/img2.jpg";
+import img4 from "./images/iron.jpg";
+
+
+
 
 export default function App() {
   return (
     <>
-      
-     {/* <Avtar/> */}
-     {/* <TodoList/> */}
 
-     <div className="flex p-10">
-      <>
-     <Box/>
-     <Box/>
-     <Box/>
-     <Box/>
-     </>
-     </div>
+      {/* <Avtar/> */}
+      {/* <TodoList/> */}
+
+      <div className="flex p-10">
+        <>
+          <Box title="box1" img={img} content="this is paragraph 1 out of 4" />
+          <Box title="box1" img={img2} content="this is paragraph 2 out of 4" />
+          <Box title="box1" img={img3} content="this is paragraph 3 out of 4" />
+          <Box title="box1" img={img4} content="this is paragraph 4 out of 4" />
+        </>
+      </div>
 
     </>
   )
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 
 
-export default function Box() {
+export default function Box({title, img, content}) {
     const [hide, SetHide] = useState(false)
 
     return (
         <div className="max-w-xs mx-auto border rounded-2xl shadow p-4">
-            <h1 onClick={() => SetHide(!hide)} className="text-xl font-bold mb-2 cursor-pointer ">Box 1</h1>
+            <h1 onClick={() => SetHide(!hide)} className="text-xl font-bold mb-2 cursor-pointer ">{title}</h1>
             <img
                 className={
                     "w-full h-40 object-cover rounded-md mb-3 " + (hide ? "invisible" : "")
                 }
-                src="https://variety.com/wp-content/uploads/2013/04/ironman3_tonystark.jpg?w=1000&h=667&crop=1"
+                src={img}
                 alt="iron man"
             />
 
@@ -20,11 +20,7 @@ export default function Box() {
                     "text-sm text-gray-700 leading-relaxed " + (hide ? "invisible" : "")
                 }
             >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-                molestiae voluptatum, dicta itaque doloribus eveniet magnam adipisci! Sunt,
-                voluptas cum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Doloremque molestiae voluptatum, dicta itaque doloribus eveniet magnam
-                adipisci! Sunt, voluptas cum.
+                {content}
             </p>
         </div>
     );
